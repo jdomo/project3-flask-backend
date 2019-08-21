cec8457eda48c045f868bd484bc7092492f40198
Joe Domovitch <jdomovitch@gmail.com>
2019-08-21 11:00:31 -0700

    add user blueprint, register in app.py

--- app.py
+++ app.py
@@ -1,13 +1,36 @@
 # 1
 from flask import Flask, g
+from flask_cors import CORS
+from flask_login import LoginManager
 
 import models
+
+from api.user import user
 
 DEBUG = True
 PORT = 5000
 
+login_manager = LoginManager()
+
 #initialize instance of Flask class
-app = Flask(__name__)
+app = Flask(__name__, static_url_path="", static_folder="static")
+
+app.secret_key = 'RANDOM STRING'
+login_manager.init_app(app)
+@login_manager.user_loader
+
+def load_user(userid):
+  try:
+    return models.user.get(models.User.id == userid)
+  except models.DoesNotExist:
+    return None
+
+CORS(api, origins=['http://localhost:3000'], supports_credentials=True)
+CORS(user, origins=['http://localhost:3000'], supports_credentials=True)
+
+app.register_blueprint(api)
+app.register_blueprint(user)
+
 
 @app.route('/')
 def index():
